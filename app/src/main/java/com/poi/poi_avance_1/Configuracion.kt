fd1696e8fe25cb12274bbab18533c56b32a099c4
Rubi <rubi170999@gmail.com>
2021-09-14 15:08:01 -0500

    Avance 1 de POI

---
+++ app/src/main/java/com/poi/poi_avance_1/Configuracion.kt
@@ -0,0 +1,12 @@
+package com.poi.poi_avance_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Configuracion : Fragment(R.layout.fragment_configuracion) {
+
+}
---
+++ app/src/main/java/com/poi/poi_avance_1/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.poi.poi_avance_1
+
+import android.app.Notification
+import android.graphics.drawable.GradientDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity()
+{
+
+    private lateinit var toogle: ActionBarDrawerToggle
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toogle=ActionBarDrawerToggle(this, drawerLayout ,R.string.open_drawer,R.string.close_drawer)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        nav_view.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_notificaciones->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, Notificaciones())
+                        commit()
+                    }
+                }
+
+
+                R.id.nav_vermensajes->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, Chats())
+                        commit()
+                    }
+                }
+
+                R.id.nav_verequipos->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, Equipos())
+                        commit()
+                    }
+                }
+
+                R.id.nav_tareas->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, Tareas())
+                        commit()
+                    }
+                }
+
+                R.id.nav_calendario->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, Calendario())
+                        commit()
+                    }
+                }
+
+                R.id.nav_configuracion->{
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, Configuracion())
+                        commit()
+                    }
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toogle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
